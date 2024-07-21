335a8716efd011bb7b4c2e9f0d34bbd13819216f
Pablo <percara.pablo@gmail.com>
2024-07-21 12:59:38 -0300

    test

--- js/main.js
+++ js/main.js
@@ -1,18 +1,96 @@
 import { a_6_8, a_8_10, a_10_12 } from './resources.js'
 
-const modal_general = document.getElementById('modal-general-texto');
-const modal_texto = document.getElementById('modal-secundario-texto');
-const btn_cerrar_titulos = document.getElementById('cerrar-modal-principal');
-const btn_cerrar_texto = document.getElementById('cerrar-modal-secundario');
+const modal_nivel_6_8 = document.getElementById('modal-nivel-6-8');
+const modal_nivel_8_10 = document.getElementById('modal-nivel-8-10');
+const modal_nivel_10_12 = document.getElementById('modal-nivel-10-12');
+const modalMetrica = document.getElementById('modal-metrica');
+const btn_cerrar_nivel_6_8 = document.getElementById('cerrar-modal-nivel-6-8');
+const btn_cerrar_nivel_8_10 = document.getElementById('cerrar-modal-nivel-8-10');
+const btn_cerrar_nivel_10_12 = document.getElementById('cerrar-modal-nivel-10-12');
+const btn_cerrar_modal_cuento = document.getElementById('cerrar-modal-secundario');
 const downloadLink = document.getElementById('downloadLink');
+const modalCuento = document.getElementById('modal-texto-cuento');
 
-btn_cerrar_titulos.addEventListener('click', () => {
-    modal_general.close();
+
+document.getElementById('btn-reiniciar').addEventListener('click', () => {
+    location.reload()
 })
 
-btn_cerrar_texto.addEventListener('click', () => {
-    modal_texto.close();
+btn_cerrar_modal_cuento.addEventListener('click', () => {
+    if (!document.getElementById('cuentos-nivel-2-6-8').classList.contains('ocultar')) {
+        document.getElementById('cuentos-nivel-2-6-8').classList.add('ocultar')
+    }
+    if (!document.getElementById('cuentos-nivel-3-6-8').classList.contains('ocultar')) {
+        document.getElementById('cuentos-nivel-3-6-8').classList.add('ocultar')
+    }
+    if (!document.getElementById('cuentos-nivel-1-6-8').classList.contains('ocultar')) {
+        document.getElementById('cuentos-nivel-1-6-8').classList.add('ocultar')
+    }
+    if (!document.getElementById('cuentos-nivel-2-8-10').classList.contains('ocultar')) {
+        document.getElementById('cuentos-nivel-2-8-10').classList.add('ocultar')
+    }
+    if (!document.getElementById('cuentos-nivel-3-8-10').classList.contains('ocultar')) {
+        document.getElementById('cuentos-nivel-3-8-10').classList.add('ocultar')
+    }
+    if (!document.getElementById('cuentos-nivel-1-8-10').classList.contains('ocultar')) {
+        document.getElementById('cuentos-nivel-1-8-10').classList.add('ocultar')
+    }
+    if (!document.getElementById('cuentos-nivel-2-10-12').classList.contains('ocultar')) {
+        document.getElementById('cuentos-nivel-2-10-12').classList.add('ocultar')
+    }
+    if (!document.getElementById('cuentos-nivel-1-10-12').classList.contains('ocultar')) {
+        document.getElementById('cuentos-nivel-1-10-12').classList.add('ocultar')
+    }
+    modalCuento.close();
 })
+
+
+btn_cerrar_nivel_6_8.addEventListener('click', () => {
+    modal_nivel_6_8.close();
+    if (!document.getElementById('cuentos-nivel-2-6-8').classList.contains('ocultar')) {
+        document.getElementById('cuentos-nivel-2-6-8').classList.add('ocultar')
+    }
+    if (!document.getElementById('cuentos-nivel-3-6-8').classList.contains('ocultar')) {
+        document.getElementById('cuentos-nivel-3-6-8').classList.add('ocultar')
+    }
+    if (!document.getElementById('cuentos-nivel-1-6-8').classList.contains('ocultar')) {
+        document.getElementById('cuentos-nivel-1-6-8').classList.add('ocultar')
+    }
+})
+
+btn_cerrar_nivel_8_10.addEventListener('click', () => {
+    modal_nivel_8_10.close();
+    if (!document.getElementById('cuentos-nivel-2-8-10').classList.contains('ocultar')) {
+        document.getElementById('cuentos-nivel-2-8-10').classList.add('ocultar')
+    }
+    if (!document.getElementById('cuentos-nivel-3-8-10').classList.contains('ocultar')) {
+        document.getElementById('cuentos-nivel-3-8-10').classList.add('ocultar')
+    }
+    if (!document.getElementById('cuentos-nivel-1-8-10').classList.contains('ocultar')) {
+        document.getElementById('cuentos-nivel-1-8-10').classList.add('ocultar')
+    }
+})
+
+btn_cerrar_nivel_10_12.addEventListener('click', () => {
+    modal_nivel_10_12.close();
+    if (!document.getElementById('cuentos-nivel-2-10-12').classList.contains('ocultar')) {
+        document.getElementById('cuentos-nivel-2-10-12').classList.add('ocultar')
+    }
+    if (!document.getElementById('cuentos-nivel-1-10-12').classList.contains('ocultar')) {
+        document.getElementById('cuentos-nivel-1-10-12').classList.add('ocultar')
+    }
+})
+
+function abrirModalMetrica() {
+    modalMetrica.showModal();
+}
+
+function insertarMetricas(tiempo, cantidad_palabras) {
+    document.getElementById('minutos').innerText = ` ${(Math.floor(tiempo / 60) % 60).toFixed(0).toString()} minutos ${(Math.round(tiempo % 60)).toFixed(0).toString()} segundos`;
+    document.getElementById('cantidad-palabras').innerText = (cantidad_palabras / tiempo).toFixed(1);
+    abrirModalMetrica();
+}
+
 let hora_inicio = 0;
 let hora_fin = 0;
 let tiempo = 0;
@@ -32,7 +110,7 @@ function SetupStream(stream) {
     }
 
     grabacion.onstop = e => {
-        const blob = new Blob(chunks, { type: "audio/mp3; codecs=opus" });
+        const blob = new Blob(chunks, { type: "audio/mp3; codecs=MPEG," });
         chunks = [];
         const audioURL = window.URL.createObjectURL(blob);
         console.log(audioURL)
@@ -40,7 +118,6 @@ function SetupStream(stream) {
         //new Date().toLocaleString()
         downloadLink.download = `audio_${new Date().toLocaleString()}.mp3`;
         downloadLink.href = audioURL;
-        downloadLink.style.display = 'block';
     }
 }
 
@@ -77,89 +154,225 @@ function grabar() {
         document.getElementById('btn-comienzo').classList.remove('btn-danger');
         document.getElementById('btn-comienzo').classList.add('btn-success');
         document.getElementById('btn-comienzo').innerText = 'Comenzar';
-
         hora_fin = Date.now();
-
-        //tiempo = ((Math.round(hora_fin - hora_inicio))/1000)/60;
-        tiempo = ((Math.round(hora_fin - hora_inicio)) / 1000);
-        console.log(tiempo)
+        //tiempo = ((Math.round(hora_fin - hora_inicio)) / 1000) / 60;
+        tiempo = (hora_fin - hora_inicio) / 1000;
+        let cantidad_palabras = document.getElementById('palabras-cuento').textContent;
+        insertarMetricas(tiempo, cantidad_palabras);
         tiempo = 0;
 
     }
 }
 
-//onclick="renderizarTexto('${data[0].cuentos[0].titulo}', '${data[0].cuentos[0].texto}')"
+function mostrarCuentosNivel_1_6_8() {
+    document.getElementById('cuentos-nivel-1-6-8').classList.toggle('ocultar');
+    if (!document.getElementById('cuentos-nivel-2-6-8').classList.contains('ocultar')) {
+        document.getElementById('cuentos-nivel-2-6-8').classList.add('ocultar')
+    }
+    if (!document.getElementById('cuentos-nivel-3-6-8').classList.contains('ocultar')) {
+        document.getElementById('cuentos-nivel-3-6-8').classList.add('ocultar')
+    }
 
-/*function renderizarTexto(titulo, texto) {
-    let contenedor = document.getElementById('contenedor-texto');
-    contenedor.innerHTML = '';
-    contenedor.innerHTML = `
-    <div class="container contenedor-texto">
-    <h4  class="titulo-texto">${titulo}</h4>
-    <p  class="texto">${texto}</p>
-    </div>
-    `
-    modal_texto.showModal();
-}*/
-
-
-
-/*function plantillaTexto(data) {
-    let plantilla = `
-    <div class="container contenedor-texto">
-    <h4  class="titulo-texto">${data.titulo}</h4>
-    <p  class="texto">${data.cuento}</p>
-    </div>
-    `
-    return plantilla;
-}*/
-
-function renderizarNivel6a8(data) {
-    let contenedor = document.getElementById('contenedor-titulos');
-    contenedor.innerHTML = '';
-    let div = document.createElement('div');
-    div.classList.add('contenedor-titulo')
-    div.innerHTML = plantillaNiveles6_8(data);
-    contenedor.append(div);
-
-    modal_general.showModal();
 }
 
-function renderizarNivel8a10(data) {
-    let contenedor = document.getElementById('contenedor-titulos');
-    contenedor.innerHTML = '';
-    let div = document.createElement('div');
-    div.classList.add('contenedor-titulo')
-    div.innerHTML = plantillaNiveles8_10(data);
-    contenedor.append(div);
+function mostrarCuentosNivel_2_6_8() {
+    document.getElementById('cuentos-nivel-2-6-8').classList.toggle('ocultar');
+    if (!document.getElementById('cuentos-nivel-1-6-8').classList.contains('ocultar')) {
+        document.getElementById('cuentos-nivel-1-6-8').classList.add('ocultar')
+    }
+    if (!document.getElementById('cuentos-nivel-3-6-8').classList.contains('ocultar')) {
+        document.getElementById('cuentos-nivel-3-6-8').classList.add('ocultar')
+    }
 
-    modal_general.showModal();
 }
 
-function renderizarNivel10a12(data) {
-    let contenedor = document.getElementById('contenedor-titulos');
-    contenedor.innerHTML = '';
-    let div = document.createElement('div');
-    div.classList.add('contenedor-titulo')
-    div.innerHTML = plantillaNiveles10_12(data);
-    contenedor.append(div);
+function mostrarCuentosNivel_3_6_8() {
+    document.getElementById('cuentos-nivel-3-6-8').classList.toggle('ocultar');
+    if (!document.getElementById('cuentos-nivel-2-6-8').classList.contains('ocultar')) {
+        document.getElementById('cuentos-nivel-2-6-8').classList.add('ocultar')
+    }
+    if (!document.getElementById('cuentos-nivel-1-6-8').classList.contains('ocultar')) {
+        document.getElementById('cuentos-nivel-1-6-8').classList.add('ocultar')
+    }
 
-    modal_general.showModal();
+}
+
+/***************************************************************************************** */
+
+
+function mostrarCuentosNivel_1_8_10() {
+
+    document.getElementById('cuentos-nivel-1-8-10').classList.toggle('ocultar');
+    if (!document.getElementById('cuentos-nivel-2-8-10').classList.contains('ocultar')) {
+        document.getElementById('cuentos-nivel-2-8-10').classList.add('ocultar')
+    }
+    if (!document.getElementById('cuentos-nivel-3-8-10').classList.contains('ocultar')) {
+        document.getElementById('cuentos-nivel-3-8-10').classList.add('ocultar')
+    }
+
+}
+
+function mostrarCuentosNivel_2_8_10() {
+
+    document.getElementById('cuentos-nivel-2-8-10').classList.toggle('ocultar');
+    if (!document.getElementById('cuentos-nivel-1-8-10').classList.contains('ocultar')) {
+        document.getElementById('cuentos-nivel-1-8-10').classList.add('ocultar')
+    }
+    if (!document.getElementById('cuentos-nivel-3-8-10').classList.contains('ocultar')) {
+        document.getElementById('cuentos-nivel-3-8-10').classList.add('ocultar')
+    }
+
+}
+
+function mostrarCuentosNivel_3_8_10() {
+
+    document.getElementById('cuentos-nivel-3-8-10').classList.toggle('ocultar');
+    if (!document.getElementById('cuentos-nivel-2-8-10').classList.contains('ocultar')) {
+        document.getElementById('cuentos-nivel-2-8-10').classList.add('ocultar')
+    }
+    if (!document.getElementById('cuentos-nivel-1-8-10').classList.contains('ocultar')) {
+        document.getElementById('cuentos-nivel-1-8-10').classList.add('ocultar')
+    }
+
+}
+/********************************************************************************************************************* */
+
+function mostrarCuentosNivel_1_10_12() {
+    console.log('prueba')
+    document.getElementById('cuentos-nivel-1-10-12').classList.toggle('ocultar');
+    if (!document.getElementById('cuentos-nivel-2-10-12').classList.contains('ocultar')) {
+        document.getElementById('cuentos-nivel-2-10-12').classList.add('ocultar')
+    }
+
+}
+
+function mostrarCuentosNivel_2_10_12() {
+    console.log('prueba')
+    document.getElementById('cuentos-nivel-2-10-12').classList.toggle('ocultar');
+    if (!document.getElementById('cuentos-nivel-1-10-12').classList.contains('ocultar')) {
+        document.getElementById('cuentos-nivel-1-10-12').classList.add('ocultar')
+    }
+
+}
+
+
+/********************************************************************************************************************** */
+function abrirModal_6_8() {
+    modal_nivel_6_8.showModal();
+}
+
+function abrirModal_8_10() {
+    modal_nivel_8_10.showModal();
+}
+
+function abrirModal_10_12() {
+    modal_nivel_10_12.showModal();
 }
 
 
 
-document.getElementById('a_6_8').addEventListener('click', () => {
-    renderizarNivel6a8(a_6_8);
+function abrirModalCuento(data) {
+    document.getElementById('titulo-cuento').innerText = data.titulo;
+    document.getElementById('texto-cuento').innerText = data.texto;
+    document.getElementById('palabras-cuento').innerText = data.palabras;
+    modalCuento.showModal();
+}
+
+document.getElementById('btn-edad-6-8').addEventListener('click', () => {
+    abrirModal_6_8();
 })
 
-document.getElementById('a_8_10').addEventListener('click', () => {
-    renderizarNivel8a10(a_8_10);
+document.getElementById('btn-edad-8-10').addEventListener('click', () => {
+    abrirModal_8_10();
 })
 
-document.getElementById('a_10_12').addEventListener('click', () => {
-    renderizarNivel10a12(a_10_12);
+document.getElementById('btn-edad-10-12').addEventListener('click', () => {
+    abrirModal_10_12();
 })
+
+document.getElementById('contenedor-nivel-1-6-8').addEventListener('click', mostrarCuentosNivel_1_6_8)
+
+document.getElementById('contenedor-nivel-2-6-8').addEventListener('click', mostrarCuentosNivel_2_6_8)
+
+document.getElementById('contenedor-nivel-3-6-8').addEventListener('click', mostrarCuentosNivel_3_6_8)
+
+document.getElementById('contenedor-nivel-1-8-10').addEventListener('click', mostrarCuentosNivel_1_8_10)
+
+document.getElementById('contenedor-nivel-2-8-10').addEventListener('click', mostrarCuentosNivel_2_8_10)
+
+document.getElementById('contenedor-nivel-3-8-10').addEventListener('click', mostrarCuentosNivel_3_8_10)
+
+document.getElementById('contenedor-nivel-1-10-12').addEventListener('click', mostrarCuentosNivel_1_10_12)
+
+document.getElementById('contenedor-nivel-2-10-12').addEventListener('click', mostrarCuentosNivel_2_10_12)
+
+
+
+/************************************************************************************************ */
+for (let index = 1; index <= 4; index++) {
+    document.getElementById('cuento-1-6-8-' + index).addEventListener('click', () => {
+        abrirModalCuento(a_6_8[0].cuentos[index - 1])
+    })
+}
+
+for (let index = 1; index <= 4; index++) {
+    document.getElementById('cuento-2-6-8-' + index).addEventListener('click', () => {
+        abrirModalCuento(a_6_8[1].cuentos[index - 1])
+    })
+}
+
+for (let index = 1; index <= 4; index++) {
+    document.getElementById('cuento-3-6-8-' + index).addEventListener('click', () => {
+        abrirModalCuento(a_6_8[2].cuentos[index - 1])
+    })
+}
+/*********************************************************************************************** */
+for (let index = 1; index <= 4; index++) {
+    if (document.getElementById('cuento-1-8-10-' + index)) {
+        document.getElementById('cuento-1-8-10-' + index).addEventListener('click', () => {
+            abrirModalCuento(a_8_10[0].cuentos[index - 1])
+        })
+    }
+
+}
+
+for (let index = 1; index <= 4; index++) {
+    if (document.getElementById('cuento-2-8-10-' + index)) {
+        document.getElementById('cuento-2-8-10-' + index).addEventListener('click', () => {
+            abrirModalCuento(a_8_10[1].cuentos[index - 1])
+        })
+    }
+
+}
+
+for (let index = 1; index <= 4; index++) {
+    if (document.getElementById('cuento-3-8-10-' + index)) {
+        document.getElementById('cuento-3-8-10-' + index).addEventListener('click', () => {
+            abrirModalCuento(a_8_10[2].cuentos[index - 1])
+        })
+    }
+
+}
+
+
+/*************************************************************************************************** */
+for (let index = 1; index <= 4; index++) {
+    if (document.getElementById('cuento-1-10-12-' + index)) {
+        document.getElementById('cuento-1-10-12-' + index).addEventListener('click', () => {
+            abrirModalCuento(a_10_12[0].cuentos[index - 1])
+        })
+    }
+}
+
+for (let index = 1; index <= 4; index++) {
+    if (document.getElementById('cuento-2-10-12-' + index)) {
+        document.getElementById('cuento-2-10-12-' + index).addEventListener('click', () => {
+            abrirModalCuento(a_10_12[1].cuentos[index - 1])
+        })
+    }
+}
+
+/************************************************************************************************** */
 
 document.getElementById('btn-comienzo').addEventListener('click', () => {
     console.log('grabar')
@@ -167,4 +380,3 @@ document.getElementById('btn-comienzo').addEventListener('click', () => {
 })
 
 
-console.log(a_6_8[0].cuentos[0])
